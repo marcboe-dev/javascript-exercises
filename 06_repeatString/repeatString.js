f4593d303021e0e960015702e424292ad84d6d8c
Marc Boehme <marc.boehme186@gmail.com>
2025-12-29 20:21:11 +0100

    solved exercises

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  if (num < 0) {
+    return "ERROR"
+  }
+  let word = "";
+  for (let i=0; i < num; i++) {
+    word += string; }
+  return word;
 };
+
+console.log(repeatString ("hey", 3));
 
 // Do not edit below this line
 module.exports = repeatString;
