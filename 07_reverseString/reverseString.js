0dd8fd74437b42850a1fc1e8fd3d45b0a4b7ca17
Marc Boehme <marc.boehme186@gmail.com>
2026-01-01 21:44:14 +0100

    finished reverseString.js

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,19 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let reversed = "";
+  const stringIndexLastLetter = string.length - 1;
+  for (let i=stringIndexLastLetter; i>=0; i--) {
+    reversed += string.charAt(i);
+  }
+  return reversed;
 };
+
+console.log(reverseString("hello"));
+
+// Pseudocode:
+// create a new empty string
+// FOR the length of the input-string, beginning from the last letter, as long as the first letter hasn't been reached, counting backwards
+// append the current letter to the new string
+// return the string
 
 // Do not edit below this line
 module.exports = reverseString;
--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...nums) {
+  let reducedArray = [];
+  array.forEach( (element) => {
+    if (!nums.includes(element) ) reducedArray.push(element);})
+  return reducedArray;
 };
 
+console.log(removeFromArray([1,2,3,4], 3, 2));
+
+
 // Do not edit below this line
+// Pseudocode:
+// function-arguments are received as a parameter
+// IF element in array is equal to nums at index i
+// delete element at index i
+// return array
+
+
 module.exports = removeFromArray;
