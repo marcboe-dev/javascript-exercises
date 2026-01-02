9f702dadbf3174ef65f0a94524d1dc7c0e3bdb5b
Marc Boehme <marc.boehme186@gmail.com>
2026-01-02 20:42:48 +0100

    finished tempConversion.js

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahrenheit) {
+  let celsius = (tempFahrenheit - 32) / (9/5); 
+  let rounded = Math.round(celsius * 10) / 10
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelsius) {
+  let fahrenheit = tempCelsius * (9/5) + 32;
+  let rounded = Math.round(fahrenheit * 10) / 10
+  return rounded;
 };
 
 // Do not edit below this line
