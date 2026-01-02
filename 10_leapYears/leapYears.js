ac992f5957d7c996a6438a149a4473c0175e4802
Marc Boehme <marc.boehme186@gmail.com>
2026-01-02 20:29:47 +0100

    finished leapYears.js

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  if (year % 4 === 0) {
+    if (year < 100) return true;
+    else if (year < 400 && year % 100 != 0) return true;
+    // check if year is a multiple of 400
+    else if (year > 399 && year % 400 === 0) return true;
+    else if (year % 100 != 0) return true;
+    else return false;
+  }
+  else return false;
 };
 
 // Do not edit below this line
